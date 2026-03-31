d5cdf69b589ac31193ef33796c5bf468c6949e80
niklas eriksson <niklaseriksson32@hotmail.com>
2026-03-31 15:14:49 +0200

    add gitignore file

--- workoutapp-API/obj/Debug/net9.0/workoutapp-API.AssemblyInfo.cs
+++ workoutapp-API/obj/Debug/net9.0/workoutapp-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("workoutapp-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00db2a0e01dc06e558ac491380beee3228d9453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d60fdf7b4d0fac563770409ed23024202d97c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("workoutapp-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("workoutapp-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
