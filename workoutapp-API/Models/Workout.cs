13ad751b4ef3ae45ef8d5a2c9e0615be00b4ae94
niklas eriksson <niklaseriksson32@hotmail.com>
2026-06-09 16:33:08 +0200

    add workout name to workouts

--- workoutapp-API/Models/Workout.cs
+++ workoutapp-API/Models/Workout.cs
@@ -10,6 +10,9 @@ public class Workout
     [Required]
     public int UserId { get; set; }
 
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public virtual ICollection<Exercise> Exercises { get; set; } = new List<Exercise>();
